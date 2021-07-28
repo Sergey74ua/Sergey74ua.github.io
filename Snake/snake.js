aaea440d5bf2bc2c93a414b9ba8f09849dbd6b43
Sergey74ua <Sergey74ua@gmail.com>
2021-07-28 13:45:30 +0300

    Update snake.js

--- Snake/snake.js
+++ Snake/snake.js
@@ -46,6 +46,17 @@ function feil() {
 	if (rS>rSmax) rSmax=rS;
 	rS=0;
 };
+//Синхронизация canvas с кадрами экрана
+window.requestAnimationFrame=( function() {
+	return	window.requestAnimationFrame		||
+			window.webkitRequestAnimationFrame	||
+			window.mozRequestAnimationFrame		||
+			window.oRequestAnimationFrame		||
+			window.msRequestAnimationFrame		||
+			function (callback) {
+				window.setTimeout(callback, 1000/15);
+			}
+})();
 
 setInterval(function() {
 	
