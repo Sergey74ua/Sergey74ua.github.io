c3a0ea3e12a3eb7cef52bd096f0bfe3af5579f6a
Sergey74ua <Sergey74ua@gmail.com>
2021-07-28 13:28:28 +0300

    up Snake

--- Snake/snake.js
+++ Snake/snake.js
@@ -5,7 +5,7 @@
  */
 
 var	snake=document.getElementById("snake"), ctx=snake.getContext("2d"), xS=snake.width, yS=snake.height,
-	qS=25, xZ=xS/2-qS, yZ=yS/2-qS, vK, xK=0, yK=0, xZarr=[], yZarr=[], arr, xF, yF, rS=0, rSmax=rS,
+	qS=25, xZ=xS/2-qS, yZ=yS/2-qS, vK, tK, xK=0, yK=0, xZarr=[], yZarr=[], arr, xF, yF, rS=0, rSmax=rS,
 	imgS=new Image(), imgF=new Image(), audFruit=new Audio(), audFeil=new Audio();
 
 imgS.src="imgSnake_01.png";
@@ -30,10 +30,10 @@ function fruit() {
 onkeydown=function(event) {
 	event.preventDefault();
 	switch(event.keyCode) {
-		case 38: case 87: if (vK!=1) {xK=0; yK=-1; vK=1}; break;
-		case 39: case 68: if (vK!=2) {xK=1; yK=0; vK=2}; break;
-		case 40: case 83: if (vK!=1) {xK=0; yK=1; vK=1}; break;
-		case 37: case 65: if (vK!=2) {xK=-1; yK=0; vK=2}; break;
+		case 38: case 87: if (vK!=1) {xK=0; yK=-1; tK=1}; break;
+		case 39: case 68: if (vK!=2) {xK=1; yK=0; tK=2}; break;
+		case 40: case 83: if (vK!=1) {xK=0; yK=1; tK=1}; break;
+		case 37: case 65: if (vK!=2) {xK=-1; yK=0; tK=2}; break;
 	}
 };
 
@@ -54,6 +54,7 @@ setInterval(function() {
 	
 	ctx.drawImage(imgF, xF-qS*0.3, yF-qS*0.35);
 	
+	vK = tK;
 	xZ=xZ+xK*qS;
 	if (xZ>=xS) xZ=0;
 	if (xZ<0) xZ=xS-qS;
