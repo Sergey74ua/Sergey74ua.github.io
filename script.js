bae7d43fe19198a286e5a77b5cddd0a5d155c355
Sergey74ua <Sergey74ua@gmail.com>
2021-09-01 02:17:20 +0300

    upgrade

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 function opentab(e, id) {
     var i, tabcontent, tablinks;
 
-    // Get all elements with class="tabcontent" and hide them
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++)
         tabcontent[i].style.display = "none";
