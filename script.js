6003cec21c88aec8a8a40606f15d8295dfed5fec
Sergey74ua <Sergey74ua@gmail.com>
2021-09-01 00:43:07 +0300

    Конкурс

---
+++ script.js
@@ -0,0 +1,17 @@
+function opentab(e, id) {
+    var i, tabcontent, tablinks;
+
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++)
+        tabcontent[i].style.display = "none";
+
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++)
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+
+    document.getElementById(id).style.display = "block";
+    e.currentTarget.className += " active";
+}
+
+document.getElementById("default").click();
